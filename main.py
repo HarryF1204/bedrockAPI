253f5722eddfca17f945590350fa940b21286036
HarryF1204 <harryf1204@protonmail.com>
2023-12-06 22:00:43 +0000

    base functionality

--- main.py
+++ main.py
@@ -1,41 +1,71 @@
 import asyncio
+import json
 import websockets
+from dataclasses import dataclass, field
+from uuid import uuid4
 
 
-class WSServerProtocol(websockets.WebSocketServerProtocol):
-    async def handle_message(self, msg):
-        # Handle messages here
-        pass
+@dataclass
+class WSServer:
+    def __init__(self, host='localhost', port=8080):
+        self._host = host
+        self._port = port
+        self._server: websockets.WebSocketServerProtocol | None = field(init=False, default=None)
 
-    async def process_messages(self):
-        # Process messages here
-        pass
+    async def handle_websocket(self, ws):
+        self._server = ws
+        await self.run_command("say a")
+        try:
+            async for msg in self._server:
+                print(msg)
+        except websockets.exceptions.ConnectionClosed as e:
+            print(e)
 
-    async def on_connect(self, request):
-        print(':: [Client Connected]')
-        await self.process_messages()
+    @staticmethod
+    def _createPayload(message_type, message_purpose, body):
+        return json.dumps({
+            "header": {
+                "version": 1,
+                "requestId": str(uuid4()),
+                "messageType": message_type,
+                "messagePurpose": message_purpose
+            },
+            "body": body
+        })
 
-    async def on_disconnect(self, close_status):
-        print(f':: [Client Disconnected]: {close_status}')
+    @classmethod
+    def _createSubscribePayload(cls, event):
+        return cls._createPayload("commandRequest", "subscribe", {
+            "eventName": event
+        })
+
+    @classmethod
+    def _createCommandPayload(cls, command):
+        return cls._createPayload("commandRequest", "commandRequest", {
+            "version": 1,
+            "origin": {"type": "player"},
+            "commandLine": command,
+            "overworld": "default"
+        })
+
+    async def subscribe(self, event):
+        payload = self._createSubscribePayload(event)
+        await self._server.send(payload)
+
+    async def run_command(self, command):
+        payload = self._createCommandPayload(command)
+        await self._server.send(payload)
+
+    def run(self):
+        print('WebSocket Server - running at')
+        print(f':: ws://localhost:{self._port}')
+        print(f':: /connect ws://{self._host}:{self._port}')
+        server = websockets.serve(self.handle_websocket, self._host, self._port)
+
+        asyncio.get_event_loop().run_until_complete(server)
+        asyncio.get_event_loop().run_forever()
 
 
-async def main():
-    host = 'localhost'
-    port = 8080
-
-    server = await websockets.serve(
-        WSServerProtocol.on_connect,
-        host=host,
-        port=port,
-        create_protocol=WSServerProtocol
-    )
-
-    print('WebSocket Server - running at')
-    print(f':: ws://{host}:{port}')
-    print(f':: /connect ws://{host}:{port}')
-    await server.wait_closed()
-
-
-if __name__ == "__main__":
-    asyncio.get_event_loop().run_until_complete(main())
-    asyncio.get_event_loop().run_forever()
+if __name__ == '__main__':
+    server = WSServer()
+    server.run()
